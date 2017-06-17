e0e9c2fc9da5192760537a6a8e0169aaa697eba3
Steven Delgado Chacón <steven.delgado.ch@gmail.com>
2017-06-17 16:11:15 -0400

    implements redis

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -10,9 +10,17 @@ class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function indexAction(Request $request)
     {
+        $redis = $this->get('snc_redis.default');
+        $redis->set('foo', 'bars');
+        $top20 = $redis->zrevrange('leaderboard', 0, 20, 'WITHSCORES');
+        $canSet = $redis->set('exclusive:onehour', 2, 'NX', 'EX', 3600);
+
+
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => 1,
